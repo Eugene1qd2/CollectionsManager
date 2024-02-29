c74bd665ab7a7fa7bbe8c0f05e313bcea8bf0bde
Eugene1qd2 <qawsed200412345@gmail.com>
2024-03-01 02:21:45 +0300

    First commit. Added login and registration. Added authorization through google. Need some fixes. Not stable yet

---
+++ CollectionManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectionManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
